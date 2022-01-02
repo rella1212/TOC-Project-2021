8c528ca2cbe4e376bca5d67f99e743a1820e249e
rella1212 <a0987920330@gmail.com>
2022-01-02 18:29:31 +0800

    Fix the new user problem

--- app.py
+++ app.py
@@ -152,112 +152,112 @@ def webhook_handler():
             continue
         if not isinstance(event.message.text, str):
             continue
+        m = 0
         for dic in user_list:
-            m = 0
             if dic.get('userID') ==  event.source.user_id:
                 m = dic.get('machine')
                 break
-            if m == 0:
-                userID = event.source.user_id
-                machine = TocMachine(
-                        states=["start", "user", "reserve", "rest_info", "meal_menu", "ask_phone", "ask_people", "ask_date", "ask_time", "check_reserve", "store1_info", "store2_info"],
-                        transitions=[
-                            {
-                                "trigger": "advance",
-                                "source": "start",
-                                "dest": "user",
-                                "conditions": "is_going_to_user",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "user",
-                                "dest": "reserve",
-                                "conditions": "is_going_to_reserve",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "user",
-                                "dest": "rest_info",
-                                "conditions": "is_going_to_rest_info",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "user",
-                                "dest": "meal_menu",
-                                "conditions": "is_going_to_meal_menu",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "rest_info",
-                                "dest": "store1_info",
-                                "conditions": "is_going_to_store1",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "rest_info",
-                                "dest": "store2_info",
-                                "conditions": "is_going_to_store2",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "reserve",
-                                "dest": "ask_phone",
-                                "conditions": "accept_name",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "ask_phone",
-                                "dest": "ask_people",
-                                "conditions": "accept_phone",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "ask_people",
-                                "dest": "ask_date",
-                                "conditions": "accept_people",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "ask_date",
-                                "dest": "ask_time",
-                                "conditions": "accept_date",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "ask_time",
-                                "dest": "check_reserve",
-                                "conditions": "accept_time",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "check_reserve",
-                                "dest": "user",
-                                "conditions": "reserve_correct",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": "check_reserve",
-                                "dest": "reserve",
-                                "conditions": "reserve_incorrect",
-                            },
-                            {
-                                "trigger": "advance",
-                                "source": ["reserve", "ask_phone", "ask_people", "ask_date", "ask_time", "check_reserve"],
-                                "dest": "user",
-                                "conditions": "cancel_reserve",
-                            },
-                            {   
-                                "trigger": "go_back",
-                                "source": ["store1_info", "store2_info", "meal_menu"],
-                                "dest": "user"
-                            },
-                        ],
-                        initial="start",
-                        auto_transitions=False,
-                        show_conditions=True,
-                    )
-                user_list.append({'userID':userID,'machine':machine})
-                m = machine
+        if m == 0:
+            userID = event.source.user_id
+            machine = TocMachine(
+                    states=["start", "user", "reserve", "rest_info", "meal_menu", "ask_phone", "ask_people", "ask_date", "ask_time", "check_reserve", "store1_info", "store2_info"],
+                    transitions=[
+                        {
+                            "trigger": "advance",
+                            "source": "start",
+                            "dest": "user",
+                            "conditions": "is_going_to_user",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "user",
+                            "dest": "reserve",
+                            "conditions": "is_going_to_reserve",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "user",
+                            "dest": "rest_info",
+                            "conditions": "is_going_to_rest_info",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "user",
+                            "dest": "meal_menu",
+                            "conditions": "is_going_to_meal_menu",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "rest_info",
+                            "dest": "store1_info",
+                            "conditions": "is_going_to_store1",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "rest_info",
+                            "dest": "store2_info",
+                            "conditions": "is_going_to_store2",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "reserve",
+                            "dest": "ask_phone",
+                            "conditions": "accept_name",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_phone",
+                            "dest": "ask_people",
+                            "conditions": "accept_phone",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_people",
+                            "dest": "ask_date",
+                            "conditions": "accept_people",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_date",
+                            "dest": "ask_time",
+                            "conditions": "accept_date",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_time",
+                            "dest": "check_reserve",
+                            "conditions": "accept_time",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "check_reserve",
+                            "dest": "user",
+                            "conditions": "reserve_correct",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "check_reserve",
+                            "dest": "reserve",
+                            "conditions": "reserve_incorrect",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": ["reserve", "ask_phone", "ask_people", "ask_date", "ask_time", "check_reserve"],
+                            "dest": "user",
+                            "conditions": "cancel_reserve",
+                        },
+                        {   
+                            "trigger": "go_back",
+                            "source": ["store1_info", "store2_info", "meal_menu"],
+                            "dest": "user"
+                        },
+                    ],
+                    initial="start",
+                    auto_transitions=False,
+                    show_conditions=True,
+                )
+            user_list.append({'userID':userID,'machine':machine})
+            m = machine
         print(f"\nFSM STATE: {m.state}")
         print(f"REQUEST BODY: \n{body}")
         
