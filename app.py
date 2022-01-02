b264f6c7006842886bc916e6a291bf2e7b530a0c
rella1212 <a0987920330@gmail.com>
2022-01-02 17:13:25 +0800

    Fix the rule of state change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request, abort, send_file
 from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageTemplateAction
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageTemplateAction, FollowEvent
 
 from fsm import TocMachine
 from utils import send_text_message
@@ -95,8 +95,14 @@ machine = TocMachine(
             "dest": "reserve",
             "conditions": "reserve_incorrect",
         },
-
-        {"trigger": "go_back",
+        {
+            "trigger": "advance",
+            "source": ["reserve", "ask_phone", "ask_people", "ask_date", "ask_time", "check_reserve"],
+            "dest": "user",
+            "conditions": "cancel reserve",
+        },
+        {   
+            "trigger": "go_back",
             "source": ["store1_info", "store2_info", "meal_menu"],
             "dest": "user"
          },
@@ -107,7 +113,7 @@ machine = TocMachine(
 )
 
 app = Flask(__name__, static_url_path="")
-
+user_list = []
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
@@ -121,33 +127,6 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
-
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
-
-    return "OK"
 
 
 @app.route("/webhook", methods=["POST"])
@@ -165,15 +144,122 @@ def webhook_handler():
 
     
     for event in events:
+        if isinstance(event, FollowEvent):
+            userID = event.source.user_id
+            machine = machine = TocMachine(
+                    states=["start", "user", "reserve", "rest_info", "meal_menu", "ask_phone", "ask_people", "ask_date", "ask_time", "check_reserve", "store1_info", "store2_info"],
+                    transitions=[
+                        {
+                            "trigger": "advance",
+                            "source": "start",
+                            "dest": "user",
+                            "conditions": "is_going_to_user",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "user",
+                            "dest": "reserve",
+                            "conditions": "is_going_to_reserve",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "user",
+                            "dest": "rest_info",
+                            "conditions": "is_going_to_rest_info",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "user",
+                            "dest": "meal_menu",
+                            "conditions": "is_going_to_meal_menu",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "rest_info",
+                            "dest": "store1_info",
+                            "conditions": "is_going_to_store1",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "rest_info",
+                            "dest": "store2_info",
+                            "conditions": "is_going_to_store2",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "reserve",
+                            "dest": "ask_phone",
+                            "conditions": "accept_name",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_phone",
+                            "dest": "ask_people",
+                            "conditions": "accept_phone",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_people",
+                            "dest": "ask_date",
+                            "conditions": "accept_people",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_date",
+                            "dest": "ask_time",
+                            "conditions": "accept_date",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "ask_time",
+                            "dest": "check_reserve",
+                            "conditions": "accept_time",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "check_reserve",
+                            "dest": "user",
+                            "conditions": "reserve_correct",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": "check_reserve",
+                            "dest": "reserve",
+                            "conditions": "reserve_incorrect",
+                        },
+                        {
+                            "trigger": "advance",
+                            "source": ["reserve", "ask_phone", "ask_people", "ask_date", "ask_time", "check_reserve"],
+                            "dest": "user",
+                            "conditions": "cancel_reserve",
+                        },
+                        {   
+                            "trigger": "go_back",
+                            "source": ["store1_info", "store2_info", "meal_menu"],
+                            "dest": "user"
+                        },
+                    ],
+                    initial="start",
+                    auto_transitions=False,
+                    show_conditions=True,
+                )
+            user_list.append({'userID':userID,'machine':machine})
+            continue
         if not isinstance(event, MessageEvent):
             continue
         if not isinstance(event.message, TextMessage):
             continue
         if not isinstance(event.message.text, str):
             continue
-        print(f"\nFSM STATE: {machine.state}")
+        for dic in user_list:
+            if dic.get('userID') ==  event.source.user_id:
+                m = dic.get('machine')
+                break
+        print(f"\nFSM STATE: {m.state}")
         print(f"REQUEST BODY: \n{body}")
-        response = machine.advance(event)
+        
+        print(f"User id: {event.source.user_id}")        
+        response = m.advance(event)
         # if response == False:
         #     send_text_message(event.reply_token, "Not Correct input")
 
--- fsm.py
+++ fsm.py
@@ -2,7 +2,7 @@ from transitions.extensions import GraphMachine
 import os
 import pygsheets
 import json
-from utils import send_text_message 
+from utils import send_text_message
 from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.models import TextSendMessage
@@ -16,7 +16,7 @@ channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
-gc = pygsheets.authorize(service_account_env_var = 'GDRIVE_API_CREDENTIALS')
+gc = pygsheets.authorize(service_account_env_var='GDRIVE_API_CREDENTIALS')
 sht = gc.open_by_url('https://docs.google.com/spreadsheets/d/1HloG5pAHXrlLvyyiHUef0qYGCQkkBJkwMSt-kqhRWcQ/edit#gid=0')
 wks_list = sht.worksheets()
 wks = sht[0]
@@ -24,7 +24,7 @@ wks = sht[0]
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
-        self.order_dict = dict(name='Allen', phone='0912345678', people=0, date='1/1',time='09:30')
+        self.order_dict = dict(name='Allen', phone='0912345678', people=0, date='1/1', time='09:30')
 
     def is_going_to_user(self, event):
         text = event.message.text
@@ -45,7 +45,7 @@ class TocMachine(GraphMachine):
     def is_going_to_store1(self, event):
         text = event.message.text
         return text.lower() == "taichung branch"
-    
+
     def is_going_to_store2(self, event):
         text = event.message.text
         return text.lower() == "tainan branch"
@@ -110,12 +110,23 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_text_message(reply_token, "https://kiarafriedphoenix.com/menu")
         self.go_back(event)
+    
+    def cancel_reserve(self, event):
+        text = event.message.text
+        if text.lower() == "cancel this reservation"
+            return True
+        else:
+            return False
 
     def accept_name(self, event):
         text = event.message.text
-        self.order_dict["name"] = text
-        return True
+        if text.lower() == "cancel this reservation":
+            return False
+        else:
+            self.order_dict["name"] = text
+            return True
     
+
     def on_enter_ask_phone(self, event):
         print("I'm entering ask phone")
         reply_token = event.reply_token
@@ -123,8 +134,11 @@ class TocMachine(GraphMachine):
 
     def accept_phone(self, event):
         text = event.message.text
-        self.order_dict["phone"] = text
-        return True
+        if text.lower() == "cancel this reservation":
+            return False
+        else:
+            self.order_dict["phone"] = text
+            return True
 
     def on_enter_ask_people(self, event):
         print("I'm entering ask people")
@@ -133,66 +147,81 @@ class TocMachine(GraphMachine):
 
     def accept_people(self, event):
         text = event.message.text
-        self.order_dict["people"] = int(text)
-        return True
-    
+        if text.lower() == "cancel this reservation":
+            return False
+        else:
+            self.order_dict["people"] = int(text)
+            return True
+
     def on_enter_ask_date(self, event):
         print("I'm entering ask date")
         reply_token = event.reply_token
-        send_text_message(reply_token, "Please enter the date you want to reserve.\nEx: 1/1\n\n[Notice]: you can only reserve the date in this month.")
+        send_text_message(
+            reply_token, "Please enter the date you want to reserve.\nEx: 1/1\n\n[Notice]: you can only reserve the date in this month.")
 
     def accept_date(self, event):
         text = event.message.text
-        date = text
-        date_list = date.split('/')
-        if date_list[0] != "1":
-            line_bot_api.push_message(event.source.user_id, TextSendMessage(text="You can only reserve the date in this month.\nPlease choose again."))
-            return False
-        elif int(date_list[1]) > 31 or int(date_list[1]) < 1:
-            line_bot_api.push_message(event.source.user_id, TextSendMessage(text="Wrong input\nPlease choose again."))
+        if text.lower() == "cancel this reservation":
             return False
         else:
-            self.order_dict["date"] = text
-            return True
+            date = text
+            date_list = date.split('/')
+            if date_list[0] != "1":
+                line_bot_api.push_message(event.source.user_id, TextSendMessage(
+                    text="You can only reserve the date in this month.\nPlease choose again."))
+                return False
+            elif int(date_list[1]) > 31 or int(date_list[1]) < 1:
+                line_bot_api.push_message(event.source.user_id, TextSendMessage(
+                    text="Wrong input\nPlease choose again."))
+                return False
+            else:
+                self.order_dict["date"] = text
+                return True
 
     def on_enter_ask_time(self, event):
         print("I'm entering ask time")
         date = self.order_dict['date']
         date_list = date.split('/')
         day = int(date_list[1])
-        day_reserve = wks.get_values(start=(2,day+1),end=(13,day+1),include_tailing_empty=True, include_tailing_empty_rows=True)
+        day_reserve = wks.get_values(start=(
+            2, day+1), end=(13, day+1), include_tailing_empty=True, include_tailing_empty_rows=True)
         s1 = 'The day reservation: '+date+'\n'
         for i in range(len(day_reserve)):
             x = day_reserve[i]
             if x == ['']:
-                if i==0:
-                    s1+='0'+str(i+9)+':30  Empty\n'
+                if i == 0:
+                    s1 += '0'+str(i+9)+':30  Empty\n'
                 else:
-                    s1+=str(i+9)+':30  Empty\n'
+                    s1 += str(i+9)+':30  Empty\n'
             else:
-                if i==0:
-                    s1+='0'+str(i+9)+':30  Reserved\n'
+                if i == 0:
+                    s1 += '0'+str(i+9)+':30  Reserved\n'
                 else:
-                    s1+=str(i+9)+':30  Reserved\n'
+                    s1 += str(i+9)+':30  Reserved\n'
         reply_token = event.reply_token
-        send_text_message(reply_token, s1+"\nPlease enter the time you want to reserve.\nEx: 9:30")
+        send_text_message(
+            reply_token, s1+"\nPlease enter the time you want to reserve.\nEx: 9:30")
 
     def accept_time(self, event):
         text = event.message.text
-        date = self.order_dict['date']
-        date_list = date.split('/')
-        day = int(date_list[1])
-        hour = text
-        hour_list = hour.split(':')
-        h = int(hour_list[0]) - 7
-        time_reserve = wks.get_value((h, day+1))
-        print(time_reserve)
-        if time_reserve == "":
-            self.order_dict["time"] = text
-            return True
-        else:
-            line_bot_api.push_message(event.source.user_id, TextSendMessage(text="This time interval is reserved.\nPlease choose another time interval."))
+        if text.lower() == "cancel this reservation":
             return False
+        else:
+            date = self.order_dict['date']
+            date_list = date.split('/')
+            day = int(date_list[1])
+            hour = text
+            hour_list = hour.split(':')
+            h = int(hour_list[0]) - 7
+            time_reserve = wks.get_value((h, day+1))
+            print(time_reserve)
+            if time_reserve == "":
+                self.order_dict["time"] = text
+                return True
+            else:
+                line_bot_api.push_message(event.source.user_id, TextSendMessage(
+                    text="This time interval is reserved.\nPlease choose another time interval."))
+                return False
 
     def on_enter_check_reserve(self, event):
         print("I'm entering check reservation")
@@ -204,43 +233,55 @@ class TocMachine(GraphMachine):
         date = "Date: " + self.order_dict['date'] + "\n"
         time = "Time: " + self.order_dict['time']
         reservation_info = reservation_info + name + people + date + time
-        line_bot_api.push_message(event.source.user_id, TextSendMessage(text=reservation_info))
+        line_bot_api.push_message(
+            event.source.user_id, TextSendMessage(text=reservation_info))
         line_bot_api.push_message(event.source.user_id, TemplateSendMessage(
-                            alt_text='Confirm template',
-                            template=ConfirmTemplate(
-                                title='Is your reservation correct?',
-                                text='Is your reservation correct?',
-                                actions=[
-                                    MessageTemplateAction(
-                                        label='Correct',
-                                        text='Correct'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='Incorrect',
-                                        text='Incorrect'
-                                    )
-                                ]
-                                )
-                            )
-                            )
+            alt_text='Confirm template',
+            template=ConfirmTemplate(
+                title='Is your reservation correct?',
+                text='Is your reservation correct?',
+                actions=[
+                    MessageTemplateAction(
+                        label='Correct',
+                        text='Correct'
+                    ),
+                    MessageTemplateAction(
+                        label='Incorrect',
+                        text='Incorrect'
+                    )
+                ]
+            )
+        )
+        )
 
     def reserve_correct(self, event):
         text = event.message.text
-        if text.lower() == "correct":
-            hour = self.order_dict['time']
-            hour_list = hour.split(':')
-            h = int(hour_list[0]) - 7
-            date = self.order_dict['date']
-            date_list = date.split('/')
-            day = int(date_list[1])
-            wks.update_value(addr=(h,day+1),val=(self.order_dict['name']+'\n'+self.order_dict['phone']+'\n'+str(self.order_dict['people'])))
-            line_bot_api.push_message(event.source.user_id, TextSendMessage(text='Your reservation is confirmed.\nHave a nice day!'))
-        return text.lower() == "correct"
+        if text.lower() == "cancel this reservation":
+            return False
+        else:
+            if text.lower() == "correct":
+                hour = self.order_dict['time']
+                hour_list = hour.split(':')
+                h = int(hour_list[0]) - 7
+                date = self.order_dict['date']
+                date_list = date.split('/')
+                day = int(date_list[1])
+                x = wks.get_value((h, day+1))
+                if x == "":
+                    wks.update_value(addr=(h, day+1), val=(self.order_dict['name']+'\n'+self.order_dict['phone']+'\n'+str(self.order_dict['people'])))
+                    line_bot_api.push_message(event.source.user_id, TextSendMessage(text='Your reservation is confirmed.\nHave a nice day!'))
+                    return True
+                else:
+                    line_bot_api.push_message(event.source.user_id, TextSendMessage(text='The reservation is failed.\nThe time you choose has been reserved.\nBack to menu.'))
+                    return True
+            else:
+                return False
 
     def reserve_incorrect(self, event):
         text = event.message.text
         if text.lower() == "incorrect":
-            line_bot_api.push_message(event.source.user_id, TextSendMessage(text='Restart the reservation process.'))
+            line_bot_api.push_message(event.source.user_id, TextSendMessage(
+                text='Restart the reservation process.'))
         return text.lower() == "incorrect"
 
     def on_enter_store1_info(self, event):
@@ -248,11 +289,9 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_text_message(reply_token, "Store1_info")
         self.go_back(event)
-    
+
     def on_enter_store2_info(self, event):
         print("I'm entering store2")
         reply_token = event.reply_token
         send_text_message(reply_token, "Store2_info")
         self.go_back(event)
-
-
